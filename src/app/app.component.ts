811fa88893572552c6050ed6816b58eca698c5d7
Will Reichl <willreichl@gmail.com>
2019-07-23 21:40:08 -0400

    S15L211: Reactive: Creating a Custom Async Validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,15 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
         // will cause error w/o bound function due to how "this" is interpreted otherwise
-        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        email: new FormControl(null, [Validators.required, Validators.email])
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this)
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        )
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([])
@@ -39,10 +47,23 @@ export class AppComponent implements OnInit {
 
   // In: Control
   // Returns key/value pair
-  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
-      return {'nameIsForbidden': true};
+      return { nameIsForbidden: true };
     }
     return null; // how you tell Angular that FormControl is valid
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
